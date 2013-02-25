d375df7ab9e0f9f7004cee0e14d7a9f80479f187
Mark Seaborn <mseaborn@chromium.org>
2013-02-25 04:08:51 +0000

    Filter out -O2 to reduce compile time from ~2s to ~1s

--- run.sh
+++ run.sh
@@ -2,12 +2,21 @@
 
 set -eu
 
+# Filter out -O2 to reduce compile time
+cflags="$(
+  for flag in $(llvm-config-3.0 --cxxflags); do
+    case "$flag" in
+      -O*) ;;
+      *) echo $flag;;
+    esac
+  done)"
+
 python test_generate_code.py > gen_arithmetic_test.c
 gcc -O1 -m32 -c gen_arithmetic_test.c
 clang -O1 -m32 -c gen_arithmetic_test.c -emit-llvm -o gen_arithmetic_test.ll
 
-g++ -m32 codegen.cc gen_arithmetic_test.o \
-  $(llvm-config-3.0 --cxxflags) \
+g++ -m32 $cflags -c codegen.cc
+g++ -m32 codegen.o gen_arithmetic_test.o \
   $(llvm-config-3.0 --ldflags --libs) -ldl \
   -UNDEBUG \
   -Wall -o codegen
