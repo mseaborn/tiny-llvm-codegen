21d56b24cefc7e2c3f5ec2df313362462a0b7ba4
Mark Seaborn <mseaborn@chromium.org>
2013-02-26 03:43:07 +0000

    Fix to ensure that assertions are enabled (broken by previous change)

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ cflags="$(
       *) echo $flag;;
     esac
   done)"
+cflags="$cflags -UNDEBUG"
 
 python test_generate_code.py > gen_arithmetic_test.c
 gcc -O1 -m32 -c gen_arithmetic_test.c
@@ -18,7 +19,6 @@ clang -O1 -m32 -c gen_arithmetic_test.c -emit-llvm -o gen_arithmetic_test.ll
 g++ -m32 $cflags -c codegen.cc
 g++ -m32 codegen.o gen_arithmetic_test.o \
   $(llvm-config-3.0 --ldflags --libs) -ldl \
-  -UNDEBUG \
   -Wall -o codegen
 
 ./codegen
